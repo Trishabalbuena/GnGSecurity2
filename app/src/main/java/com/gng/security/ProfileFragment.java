924f0b59f7d214b13ca073a6647a056ecbe80ab1
Trisha Balbuena <trishabalbuena22@gmail.com>
2025-11-25 21:46:27 +0800

    Initial commit.

--- app/src/main/java/com/gng/security/ProfileFragment.java
+++ app/src/main/java/com/gng/security/ProfileFragment.java
@@ -1,13 +1,9 @@
 package com.gng.security;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,22 +14,6 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
-
-        Button logoutButton = view.findViewById(R.id.logout_button);
-        logoutButton.setOnClickListener(v -> {
-            // Clear the login state
-            SharedPreferences sharedPreferences = getActivity().getSharedPreferences("GnGSecurityPrefs", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putBoolean("isLoggedIn", false);
-            editor.apply();
-
-            // Go back to LoginActivity
-            Intent intent = new Intent(getActivity(), LoginActivity.class);
-            startActivity(intent);
-            getActivity().finish(); // Finish MainActivity so the user can't go back
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
